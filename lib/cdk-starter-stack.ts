c49292134cf07554eca65582e0e1c74e5803d012
deimerhdz <deimerhdz@gmail.com>
2024-10-10 20:05:46 -0500

    First commit

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,16 +1,47 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+class L3BUcket extends Construct {
+  constructor(scope: Construct, id: string,expiration:number){
+    super(scope,id)
+    new Bucket(this,id,{
+      lifecycleRules:[{
+        expiration:cdk.Duration.days(expiration)
+      }]
+    })
+  }
+}
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+// create an s3 bucket 3 ways 
+    // L1
+    new CfnBucket(this,'startterBucketL1',{
+      lifecycleConfiguration:{
+        rules:[{
+          expirationInDays:1,
+          status:'Enabled'
+        }]
+      }
+    })
+    const duration = new cdk.CfnParameter(this,'duration',{
+      default:6,
+      minValue:1,
+      maxValue:10,
+      type:'Number'
+    })
+    //L2
+    const myL2Bucket = new Bucket(this,'starterBucketL2',{
+      lifecycleRules:[{
+        expiration:cdk.Duration.days(duration.valueAsNumber)
+      }]
+    })
+    
+    //L3
+    new L3BUcket(this,'starterBucketL3',3)
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStarterQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this,'MyL2BucketName',{
+      value:myL2Bucket.bucketName
+    })
   }
 }
